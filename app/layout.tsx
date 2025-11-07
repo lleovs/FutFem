3bcbe632978b77d6175dee250856975b5526d5bf
Leonardo Vinícius <leonardo.v.souzafilho@gmail.com>
2025-11-07 17:53:34 -0300

    commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -12,24 +12,7 @@ export const metadata: Metadata = {
   title: "FutFem",
   description: "",
   generator: "",
-  icons: {
-    icon: [
-      {
-        url: "/icon-light-32x32.png",
-        media: "(prefers-color-scheme: light)",
-      },
-      {
-        url: "/icon-dark-32x32.png",
-        media: "(prefers-color-scheme: dark)",
-      },
-      {
-        url: "/icon.svg",
-        type: "image/svg+xml",
-      },
-    ],
-    apple: "/apple-icon.png",
-  },
-}
+
 
 export default function RootLayout({
   children,
