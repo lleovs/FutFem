cc6f25007f8dea7302c61554b0e16446771f478f
Leonardo Vinícius <leonardo.v.souzafilho@gmail.com>
2025-11-07 17:52:13 -0300

    projeto final

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,15 +1,15 @@
-import type React from "react"
+import { ThemeProvider } from "@/lib/theme-provider"
+import { Analytics } from "@vercel/analytics/next"
 import type { Metadata } from "next"
 import { Geist, Geist_Mono } from "next/font/google"
-import { Analytics } from "@vercel/analytics/next"
-import { ThemeProvider } from "@/lib/theme-provider"
+import type React from "react"
 import "./globals.css"
 
 const _geist = Geist({ subsets: ["latin"] })
 const _geistMono = Geist_Mono({ subsets: ["latin"] })
 
 export const metadata: Metadata = {
-  title: "v0 App",
+  title: "FutFem",
   description: "Created with v0",
   generator: "v0.app",
   icons: {
